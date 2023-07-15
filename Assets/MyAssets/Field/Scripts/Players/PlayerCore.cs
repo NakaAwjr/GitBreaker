9881ff669f4a6023d2fd4cafd14114396c4d634d
woods-one <0820ricer@gmail.com>
2023-07-15 23:49:59 +0900

    変更 : Playerにダメージが与えられるように変更しました

--- Assets/MyAssets/Field/Scripts/Players/PlayerCore.cs
+++ Assets/MyAssets/Field/Scripts/Players/PlayerCore.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Assets.MyAssets.Field.Scripts.Damages;
 using Assets.MyAssets.Field.Scripts.States;
 using UniRx;
 using UniRx.Triggers;
@@ -11,7 +12,7 @@ namespace Assets.MyAssets.Field.Scripts.Players
     /// <summary>
     /// プレイヤーの基本情報
     /// </summary>
-    public class PlayerCore : MonoBehaviour
+    public class PlayerCore : MonoBehaviour, IDamageable
     {
         private PlayerId _playerId;
         public PlayerId PlayerId => _playerId;
@@ -39,27 +40,36 @@ namespace Assets.MyAssets.Field.Scripts.Players
         private ReactiveProperty<PlayerGear> _currentPlayerGear;
         public IReadOnlyReactiveProperty<PlayerGear> CurrentPlayerGear => _currentPlayerGear;
         
+        private Subject<Damage> _damageSubject = new Subject<Damage>();
+        public IObservable<Damage> OnDamaged { get { return _damageSubject; } }
 
         void Awake()
         {
             InitializePlayer();
             
-            _onInitializeAsyncSubject.Subscribe(x =>
-            {
-                _currentPlayerParameter = new ReactiveDictionary<string,int>()
+            OnDamaged.Where(x => 0 < x.AttackValue - _currentPlayerParameter["Defence"])
+                .Subscribe(x =>
                 {
-                    {"Hp", x.Hp}, 
-                    {"Power", x.Power}, 
-                    {"Defence", x.Defence},
-                    {"MagicPoint", x.MagicPoint},
-                    {"MagicPower", x.MagicPower},
-                    {"MagicDefence", x.MagicDefence},
-                    {"Speed", x.Speed}
-                };
-                
-                IsAlive.Where(y => y).Skip(1)
-                    .Subscribe(_ => { Debug.Log("いくぞ！"); });
-            });             
+                    _currentPlayerParameter["Hp"] -= x.AttackValue - _currentPlayerParameter["Defence"];
+                });
+            
+            _onInitializeAsyncSubject
+                .Subscribe(x =>
+                {
+                    _currentPlayerParameter = new ReactiveDictionary<string,int>()
+                    {
+                        {"Hp", x.Hp}, 
+                        {"Power", x.Power}, 
+                        {"Defence", x.Defence},
+                        {"MagicPoint", x.MagicPoint},
+                        {"MagicPower", x.MagicPower},
+                        {"MagicDefence", x.MagicDefence},
+                        {"Speed", x.Speed}
+                    };
+                    
+                    IsAlive.Where(y => y).Skip(1)
+                        .Subscribe(_ => { Debug.Log("いくぞ！"); });
+                });             
             
             this.OnTriggerEnter2DAsObservable()
                 .Where(__ => IsAlive.Value)
@@ -73,6 +83,12 @@ namespace Assets.MyAssets.Field.Scripts.Players
                     }*/
                 });
         }
+        
+        public void DealDamage(Damage damage)
+        {
+            _damageSubject.OnNext(damage);
+        }
+
 
         public void ResetPlayerParameter()
         {
