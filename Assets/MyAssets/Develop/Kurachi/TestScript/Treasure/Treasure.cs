57d92d497feb4fea4ca39ba10210d3bc31d15fd8
sirin1001 <mokunosirin1001@gmail.com>
2023-07-16 23:58:35 +0900

    作成:宝箱オブジェクトの作成とテスト用のシーンの作成

---
+++ Assets/MyAssets/Develop/Kurachi/TestScript/Treasure/Treasure.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    [SerializeField] Sprite CloseFolder;
+    [SerializeField] Sprite OpenFolder;
+    [SerializeField] List<GameObject> Item = new List<GameObject>();
+    int _dropItemMaxCnt=3;
+    bool _isUsed = false;
+    
+    void Start(){
+        this.GetComponent<SpriteRenderer>().sprite = CloseFolder;
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("OnTrigger");
+        if(!_isUsed){
+            UseTreasure();
+        }
+        
+    }
+    public void UseTreasure(){
+        SetIsUsed();
+        ChangeSprite();
+        DropItem();
+    }
+    void SetIsUsed(){
+        _isUsed = true;
+    }
+    void ChangeSprite(){
+        this.GetComponent<SpriteRenderer>().sprite = OpenFolder;
+    }
+    void DropItem(){
+        for(int i=0; i<Random.Range(1,_dropItemMaxCnt); i++){
+            var pos = transform.position;
+            pos.x += Random.Range(-3f, 3f);
+            pos.y += Random.Range(-3f, 3f);
+            Instantiate(Item[Random.Range(0,Item.Count)], pos, transform.rotation);
+        }
+        
+    }
+    public bool GetIsUsed(){
+        return _isUsed;
+    }
+}
