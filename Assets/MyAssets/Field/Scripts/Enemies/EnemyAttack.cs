8807b25ef7240b7383aec8254bd420fda4db70cf
woods-one <0820ricer@gmail.com>
2023-07-17 08:11:34 +0900

    変更 : エネミーの挙動の調整を行いました

--- Assets/MyAssets/Field/Scripts/Enemies/EnemyAttack.cs
+++ Assets/MyAssets/Field/Scripts/Enemies/EnemyAttack.cs
@@ -7,10 +7,14 @@ namespace Assets.MyAssets.Field.Scripts.Enemies
 {
     public class EnemyAttack : BaseEnemyComponent
     {
+        [SerializeField]
+        private EnemyCollision _enemyCollision;
+        
         protected override void OnInitialize()
         {
             Observable.Timer(System.TimeSpan.Zero, System.TimeSpan.FromSeconds(5))
-                .Subscribe(x =>
+                .Where(_ => _enemyCollision.CanAttack)
+                .Subscribe(_ =>
                     {
                         EnemyCore.CurrentEnemyGear.Value.EnemyWeapon.AttackNormal(EnemyCore.CurrentEnemyParameter);
                     }
