2f2b3250857c19f8a73520172a795c00435d5eea
NakaAwjr <n.sadanana@gmail.com>
2023-07-17 10:14:02 +0900

    UIに値代入

--- Assets/MyAssets/Develop/Nakamura/UI/UIStatusValues.cs
+++ Assets/MyAssets/Develop/Nakamura/UI/UIStatusValues.cs
@@ -1,25 +1,19 @@
+using Assets.MyAssets.Field.Scripts.Players;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static Unity.Burst.Intrinsics.X86.Avx;
+using UniRx;
+
 
 public class UIStatusValues : MonoBehaviour
 {
-    private int _maxhp;
-    private int _hp;
-    private int _maxmp;
-    private int _mp;
-    private int _power;
-    private int _defense;
-    private int _magicpower;
-    private int _magicdefense;
-    private int _speed;
+    [SerializeField]
+    PlayerCore _playerCore;
 
-    private string _currentwepon;
-    private string _currenthead;
-    private string _currentbody;
-    private string _currentbottom;
+    private List<int> _status;
 
     [SerializeField] private TMP_Text _hpText;
     [SerializeField] private TMP_Text _mpText;
@@ -34,26 +28,53 @@ public class UIStatusValues : MonoBehaviour
 
     void Start()
     {
-        SetStatus();
-    }
-
-    public void SetStatus()
-    {
-        _hpText.text = "HP(" + _hp.ToString() + "/" + _maxhp.ToString() + ")";
-        _mpText.text = "MP(" + _mp.ToString() + "/" + _maxmp.ToString() + ")";
-        _statusText.text = "Power:" + _power.ToString() + "\n" +
-                           "Defense:" + _defense.ToString() + "\n" +
-                           "MagicPower:" + _magicpower.ToString() + "\n" +
-                           "MagickDefense:" + _magicdefense.ToString() + "\n" +
-                           "Speed:" + _speed.ToString();
-        _hpSlider.maxValue = _maxhp;
-        _hpSlider.value = _hp;
-        _mpSlider.maxValue = _maxmp;
-        _mpSlider.value = _mp;
-
-        _weponTexr.text = "Wepon:" + _currentwepon;
-        _headText.text = "Head:" + _currenthead;
-        _bodyText.text = "Body:" + _currentbody;
-        _bottomText.text = "Bottom:" + _currentbottom;
+        _playerCore.CurrentPlayerParameter.ObserveReplace().Subscribe(x =>
+        {
+            switch(x.Key)
+            { 
+                case "Hp":
+                    _hpText.text = "HP(" + x.NewValue + "/" + 10 + ")";
+                    _hpSlider.value = x.NewValue;
+                    break;
+                case "Mp":
+                    _mpText.text = "MP(" + x.NewValue + "/" + 10 + ")";
+                    _mpSlider.value = x.NewValue;
+                    break;
+                case "Power":
+                    _status[0] = x.NewValue;
+                    break;
+                case "Defence":
+                    _status[1] = x.NewValue;
+                    break;
+                case "MagicPower":
+                    _status[2] = x.NewValue;
+                    break;
+                case "MagicDefence":
+                    _status[3] = x.NewValue;
+                    break;
+                case "Speed":
+                    _status[4] = x.NewValue;
+                    break;
+                case "Wepon":
+                    _weponTexr.text = "Wepon:" + x.NewValue;
+                    break;
+                case "Head":
+                    _headText.text = "Head:" + x.NewValue;
+                    break;
+                case "Body":
+                    _bodyText.text = "Body:" + x.NewValue;
+                    break;
+                case "Bottom":
+                    _bottomText.text = "Bottom:" + x.NewValue;
+                    break;
+                default:
+                    break;
+            }
+            _statusText.text = "Power:" + _status[0] + "\n" +
+                               "Defence:" + _status[1] + "\n" +
+                               "MagicPower:" + _status[2] + "\n" +
+                               "MagicDefence:" + _status[3] + "\n" +
+                               "Speed:" + _status[4];
+        });
     }
 }
